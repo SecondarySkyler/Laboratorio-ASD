30252a6a8fe69a839b29cccd47e94cfd8bf6aea3
Cristian Murtas <skylerhunter6@gmail.com>
2020-11-17 14:52:04 +0100

    added exercises

---
+++ src/Exercises/nSum.java
@@ -0,0 +1,12 @@
+package Exercises;
+
+public class nSum {
+
+
+
+    public static void main(String[] args) {
+        int[] a = {5, 0, 3, 1, 13, 7, 5};
+        int key = 17;
+
+    }
+}
--- src/Exercises/twoSum.java
+++ src/Exercises/twoSum.java
@@ -2,4 +2,51 @@ package Exercises;
 
 public class twoSum {
 
+
+    /**
+     * return the 2 indices i and j so that a[i] + a[j] = k
+     * @param a array where to find the 2 indices
+     * @param k the key
+     *          TIME COMPLEXITY O(n^2)
+     */
+    public static void trovaIndici(int[] a, int k) {
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = 1; j < a.length; j++) {
+                if (a[i] +  a[j] == k) {
+                    System.out.println(""+ i + ", " + j);
+                }
+            }
+        }
+        System.out.println("not found");
+    }
+
+    /**
+     * Search two indices i and j so that a[i] + a[j] = k
+     * @param a the array where to search
+     * @param key the key to find
+     *            TIME COMPLEXITY O(n)
+     */
+    public static void twoSum(int[]a , int key) {
+        int i = 0;
+        int j = a.length - 1;
+        while (i < j) {
+            if (a[i] + a[j] == key) {
+                System.out.println("" + i + ", " + j);
+                return;
+            }
+            else if (a[i] + a[j] < key)
+                i++;
+            else
+                j--;
+        }
+        System.out.println("not found");
+    }
+
+
+    public static void main(String[] args) {
+        int[] a = {3, 7, 8, 11, 15, 20, 24};
+        int key = 23;
+        trovaIndici(a,key);
+        twoSum(a,key);
+    }
 }
