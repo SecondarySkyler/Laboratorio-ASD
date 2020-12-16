f17301cc1b080e4c1956cf17f28dd2b8cc43258d
Cristian Murtas <skylerhunter6@gmail.com>
2020-12-16 10:18:16 +0100

    Added methods: - addNode -maxKey

--- src/BinarySearchTree/BST_Rappresentation.java
+++ src/BinarySearchTree/BST_Rappresentation.java
@@ -5,22 +5,43 @@ public class BST_Rappresentation {
     public Node root;
 
 
-    public BST_Rappresentation(Node n) {
-        this.root = n;
+    public BST_Rappresentation(int n) {
+       this.root = new Node(n);
     }
 
-    public Node addNode(Node root, int key) {
-        while (root != null) {
-            if (root.getKey() < key) {
-                addNode(root.getRight(), key);
+    /**
+     * method to add a node into a bst
+     * @param key of the node to insert
+     * @return a bst with node z with z.getKey() = key
+     */
+    public void addNode(int key) {
+        Node y = null;
+        Node x = this.root;
+
+        while (x != null) {
+            y = x;
+            if (x.getKey() > key) {
+                x = x.getLeft();
             } else {
-                addNode(root.getLeft(), key);
+                x = x.getRight();
             }
         }
-        root = new Node(key);
-        return root;
+        Node z = new Node(key);
+        z.parent = y;
+        if (y.getKey() > z.getKey()) {
+            y.left = z;
+        } else {
+            y.right = z;
+        }
     }
 
+    /**
+     * method to search for a key in a bst
+     * @param root of the bst
+     * @param key to search for
+     * @return the key if is in the bst
+     * @throws NullPointerException is key is not in the bst
+     */
     public Node search (Node root, int key) {
         if(root == null || root.getKey() == key)
             return root;
@@ -29,5 +50,21 @@ public class BST_Rappresentation {
         } else {
             return search(root.getLeft(), key);
         }
+    }
+
+    /**
+     *
+     * @return the max key in the bst
+     */
+    public int maxKey() {
+        return maxKeyRecursive(this.root);
+    }
+
+    private int maxKeyRecursive(Node n) {
+        while (n.right != null) {
+            n = n.getRight();
+            maxKeyRecursive(n);
+        }
+        return n.getKey();
     }
 }
