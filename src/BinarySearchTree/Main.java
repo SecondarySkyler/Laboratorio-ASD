16eb91e3b9874d0e5df4a481e9847931832bac13
Cristian Murtas <skylerhunter6@gmail.com>
2020-12-16 10:18:30 +0100

    Added test

--- src/BinarySearchTree/Main.java
+++ src/BinarySearchTree/Main.java
@@ -3,9 +3,27 @@ package BinarySearchTree;
 public class Main {
 
     public static void main(String[] args) {
-        Node first = new Node(5);
-        BST_Rappresentation tree = new BST_Rappresentation(first);
-        Node searched = tree.search(tree.root, 5);
-        System.out.print(searched.getKey());
+
+        BST_Rappresentation tree = new BST_Rappresentation(15);
+        tree.addNode(17);
+        tree.addNode(10);
+        tree.addNode(12);
+        /**
+         *          15
+         *         /  \
+         *        10   17
+         *       /  \
+         *           12
+         */
+        try {
+            Node searched = tree.search(tree.root, 12);
+            System.out.println(searched.getKey());
+        } catch (NullPointerException npe) {
+            System.out.print("Nodo non trovato");
+        }
+
+        System.out.print(tree.maxKey());
+
+
     }
 }
